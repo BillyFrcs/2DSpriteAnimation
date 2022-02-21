5c94c1695345844cb5552843dd4c67fb54ff7ceb
Billy Franscois <billyfranscois@outlook.com>
2022-02-21 12:43:02 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "Game.hpp"
 
+class Game;
+
 int main(void)
 {
     Billy::Game *GameAnimation = new Billy::Game();
 
-    GameAnimation->RunGameAnimation();
+    GameAnimation->Run();
 
     return EXIT_SUCCESS;
 }
